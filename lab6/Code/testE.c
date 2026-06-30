07765e9e196413f2fe80f16a1137cced3e1c23f9
Fabian <e12510804@student.tuwien.ac.at>
2026-06-30 12:20:42 +0200

    lab_6 angabe && code fixed lab6 taskE

--- lab6/Code/testE.c
+++ lab6/Code/testE.c
@@ -1,42 +1,61 @@
-#include <stdio.h>
-#include <string.h>
+#include <assert.h>
 #include <stddef.h>
-#include <ctype.h>
+#include <string.h>
 
+#define NUM_USERS 4
 
-struct User{
-    int uid;
-    char email[128];
-    char name[128];
-};
+typedef struct
+{
+  unsigned int uid;
+  const char *email;
+  const char *name;
+} User;
 
+void validate(const User *users, size_t n, int *output)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    int invalid_email = 1; // 1 -> email is not valid
+    size_t j = 0;
 
+    while (users[i].email[j] != '\0')
+    {
+      if (users[i].email[j] == '@')
+        invalid_email = 0;
+      j++;
+    }
 
+    int unique_uid = 1;
+    for (size_t k = 0; k < n; k++)
+    {
+      if (k != i && users[k].uid == users[i].uid)
+      {
+        unique_uid = 0;
+        break;
+      }
+    }
 
+    if (unique_uid && users[i].name[0] != '\0' && invalid_email == 0)
+      output[i] = 1;
+    else
+      output[i] = 0;
+  }
+}
 
+int main()
+{
+  User users[NUM_USERS] = {
+      {1, "e123@cpp.at", "e123"},  // valid
+      {2, "e124@cpp.at", ""},      // empty name
+      {3, "e1245cpp.at", "e1245"}, // invalid email
+      {2, "e125@cpp.at", "e125"}   // duplicate uid
+  };
 
+  int res[NUM_USERS];
+  validate(users, NUM_USERS, res);
 
+  int expected[NUM_USERS] = {1, 0, 0, 0};
+  assert(memcmp(res, expected, sizeof(expected)) == 0);
 
-struct User user1 = {1, "e123@cpp.at", "e123"};
-struct User user2 = {2, "e124@cpp.at", ""};
-struct User user3 = {3, "e1245cpp.at", "e1245"};
-struct User user4 = {2, "e125@cpp.at", "e125"};
-
-
-
-//struct User users[4] = {user1, user2, user3, user4};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return 0;
+}
--- lab6/Code/testE.cpp
+++ lab6/Code/testE.cpp
@@ -4,40 +4,57 @@
 #include <string>
 #include <vector>
 
-struct User {
- unsigned int uid;
- std::string email;
- std::string name;
+struct User
+{
+  unsigned int uid;
+  std::string email;
+  std::string name;
 };
 
-std::vector<bool> validate(std::vector<User> users){
-    std::vector<bool> output;
+std::vector<bool> validate(std::vector<User> users)
+{
+  std::vector<bool> output;
 
-    unsigned int invalid_email = 1;     //true -> email in not valid
-    size_t j = 0;
+  for (size_t i = 0; i < users.size(); i++)
+  {
+    unsigned int invalid_email = 1; // true -> email is not valid
+    size_t j = 0;                   // j muss pro Nutzer neu starten
 
-    for (size_t i = 0; i < users.size(); i++){
-        while (users[i].email[j] != '\0') {
-            if (users[i].email[j] == '@') invalid_email = 0;
-            j++;
-        }
-        if ((users[i].uid == i+1) && users[i].name[0] != '\0' && invalid_email == 0) output.push_back(true);    //only checking seqential (autogengen uid) and not duplicate uid, system relies onstacking uid 
-        else output.push_back(false);
-        invalid_email = 1;
-        j = 0;
+    while (users[i].email[j] != '\0')
+    {
+      if (users[i].email[j] == '@')
+        invalid_email = 0;
+      j++;
     }
-    return output;
+
+    bool unique_uid = true;
+    for (size_t k = 0; k < users.size(); k++)
+    {
+      if (k != i && users[k].uid == users[i].uid)
+      {
+        unique_uid = false;
+        break;
+      }
+    }
+
+    if (unique_uid && users[i].name[0] != '\0' && invalid_email == 0)
+      output.push_back(true);
+    else
+      output.push_back(false);
+  }
+  return output;
 }
 
-int main() {
- std::vector<User> users = {
- {1, "e123@cpp.at", "e123"}, // valid
- {2, "e124@cpp.at", ""}, // empty name
- {3, "e1245cpp.at", "e1245"}, // invalid email
- {2, "e125@cpp.at", "e125"} // duplicate uid
- };
- auto res = validate(users);
- std::vector<bool> expected = {true, false, false, false};
- assert(res == expected);
- return 0;
+int main()
+{
+  std::vector<User> users = {
+      {1, "e123@cpp.at", "e123"},  // valid
+      {2, "e124@cpp.at", ""},      // empty name
+      {3, "e1245cpp.at", "e1245"}, // invalid email
+      {2, "e125@cpp.at", "e125"}   // duplicate uid
+  };
+  auto res = validate(users);
+  std::vector<bool> expected = {true, false, false, false};
+  assert(res == expected);
+  return 0;
 }
